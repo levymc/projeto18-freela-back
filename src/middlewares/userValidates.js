f2c46bf3bba482f987045b0e6410b06d2d2140ba
Levy <levymcruz@gmail.com>
2023-08-14 15:33:53 -0300

    uservalidate remove dao

--- src/middlewares/userValidates.js
+++ src/middlewares/userValidates.js
@@ -1,11 +1,6 @@
-import UsuarioDAO from "../prisma/dao/dao.users.js";
 import { crypt, compare } from "./crypt.js";
-import LinkDAO from "../prisma/dao/dao.links.js";
 import  { PrismaClient } from '@prisma/client';
 
-
-const daoLink = new LinkDAO()
-const dao = new UsuarioDAO()
 
 const prisma = new PrismaClient();
 
@@ -28,23 +23,3 @@ export async function validateUserPass(req, res, next){
         return res.status(500).send("Erro Validação de Email e Senha: ",err)
     }
 }
-
-export async function checkShortUrlUserID(req, res, next) {
-    try {
-        const userId = parseInt(res.user.id);
-        const urlId = parseInt(req.params.id);
-        console.log("USER: ", userId);
-        const checkUserID = await daoLink.readByCreatedBy(userId);
-        const readByUrlId = await daoLink.readById(urlId);
-
-        if(!readByUrlId) return res.sendStatus(404)
-        if (!checkUserID) return res.status(404).send("Este usuário não possui nenhuma ShortUrl.");
-        const urlExistsForUser = checkUserID.some(urlObject => urlObject.id === urlId);
-        if (!urlExistsForUser) return res.sendStatus(401)    
-        next();
-    } catch (err) {
-        console.error("Erro checkShortUrlUserID: ", err);
-        return res.status(500).send("Erro checkShortUrlUserID: ", err);
-    }
-}
-  
